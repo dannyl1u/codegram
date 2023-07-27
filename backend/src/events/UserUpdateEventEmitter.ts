6fa89583f4590c1d1a3616f3ed30a6f51e76fc99
George Shramko <shramko.georgiy@gmail.com>
2023-07-26 18:30:16 -0700

    Integrating Google Pub/Sub

--- backend/src/events/UserUpdateEventEmitter.ts
+++ backend/src/events/UserUpdateEventEmitter.ts
@@ -1,5 +1,8 @@
 import EventEmitter from "events";
 import { UpdateEventData, EventRepository } from "../repository/EventRepository";
+import path from 'path'
+import { env } from "../config/env";
+import { PubSub } from "@google-cloud/pubsub";
 
 export class UserUpdateEventEmitter {
   private eventEmitter: EventEmitter;
@@ -11,11 +14,32 @@ export class UserUpdateEventEmitter {
     this.eventRepository = new EventRepository();
     //TODO: setup things for GCP Pub/Sub
 
+    const secret_key_filepath = path.join(__dirname, '..', '..', 'secrets', env.GCP_SECRET_FILE!!);
+    const projectId = env.GCP_PROJECT_ID;
+    const topicName = env.GCP_PUBSUB_UPDATE_TOPIC!!;
+
+    // Creates a client with the specified credentials as a string.
+  const pubSubClient = new PubSub({ projectId: projectId, keyFilename: secret_key_filepath});
+
+  // Get a reference to the topic.
+  const topic = pubSubClient.topic(topicName);
+
+  const data = JSON.stringify({ key: 'value' });
+
+    
     this.eventEmitter.on(
       this.EVENT_NAME,
       async (updateData: UpdateEventData) => {
-        console.log("Update event fired", updateData);
-        await this.eventRepository.saveEvent(updateData);
+        try {
+          console.log("Update event fired", updateData);
+          // await this.eventRepository.saveEvent(updateData);
+        
+          const messageId = await topic.publishMessage({json:updateData});
+          // Publish the message.
+          console.log(`Message ${messageId} published.`);
+        } catch(err) {
+          console.log(err);
+        }
       }
     );
   }
